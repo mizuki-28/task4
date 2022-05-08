15d6fc090579e6f206b5e96cb368e286aa41602b
mizuki <mtc_m@icloud.com>
2022-05-08 22:56:04 +0900

    初回提出

---
+++ memo.rb
@@ -0,0 +1,22 @@
+require "csv"
+puts "1(新規でメモを作成) 2(既存のメモを編集する)"
+memo_type = gets.to_s
+input_number = memo_type.chomp
+
+if input_number == "1"
+  puts "拡張子を除いたファイルを入力してください"
+  csv_title = gets.chomp
+  p "メモしたい内容を記入してください"
+  p "完了したらCtrl + Dをおします"
+  input_memo = $stdin.read
+  memo = input_memo.chomp
+  CSV.open("#{csv_title}.csv" , 'w') do |csv|
+  csv.puts ["#{memo}"]
+  end
+
+elsif input_number == "2"
+  puts "編集したい拡張子を除いたファイル名を入力してください"
+
+else
+  puts "1か2を入力してください"
+end
