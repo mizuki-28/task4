97aee6bf633e4ab3ac013da41f07c6397f4b70f7
mizuki <mtc_m@icloud.com>
2022-05-12 20:16:02 +0900

    既存メモ編集機能の追加

--- memo.rb
+++ memo.rb
@@ -6,16 +6,24 @@ input_number = memo_type.chomp
 if input_number == "1"
   puts "拡張子を除いたファイルを入力してください"
   csv_title = gets.chomp
-  p "メモしたい内容を記入してください"
-  p "完了したらCtrl + Dをおします"
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dをおします"
   input_memo = $stdin.read
   memo = input_memo.chomp
   CSV.open("#{csv_title}.csv" , 'w') do |csv|
   csv.puts ["#{memo}"]
-  end
+end
 
 elsif input_number == "2"
   puts "編集したい拡張子を除いたファイル名を入力してください"
+  csv_title = gets.chomp
+  puts "メモしたい内容を記入してください"
+  puts "完了したらCtrl + Dをおします"
+  input_memo = $stdin.read
+  memo = input_memo.chomp
+  CSV.open("#{csv_title}.csv" , 'a') do |csv|
+  csv.puts ["#{memo}"]
+end
 
 else
   puts "1か2を入力してください"
